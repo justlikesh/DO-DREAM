89017594d6308c5571f8a28663b7169503d19166
양진서 <jinseo_yang@naver.com>
2025-10-27 15:22:04 +0900

    feat: Navigation type 정의 (S13P31A704-84)

---
+++ fe_app/src/navigation/navigationTypes.ts
@@ -0,0 +1,42 @@
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { RouteProp } from '@react-navigation/native';
+import { Book } from '../data/dummyBooks';
+
+export type RootStackParamList = {
+  Library: undefined;
+  PlaybackChoice: {
+    book: Book;
+  };
+  Player: {
+    book: Book;
+    chapterId: string;
+    fromStart: boolean;
+  };
+};
+
+// Navigation prop 타입
+export type LibraryScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Library'
+>;
+
+export type PlaybackChoiceScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'PlaybackChoice'
+>;
+
+export type PlayerScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Player'
+>;
+
+// Route prop 타입
+export type PlaybackChoiceScreenRouteProp = RouteProp<
+  RootStackParamList,
+  'PlaybackChoice'
+>;
+
+export type PlayerScreenRouteProp = RouteProp<
+  RootStackParamList,
+  'Player'
+>;
