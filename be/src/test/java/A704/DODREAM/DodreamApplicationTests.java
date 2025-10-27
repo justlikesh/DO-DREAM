ca63db7eb06316545d867757b4c8df25c664c967
김승호 <lpsac@naver.com>
2025-10-27 10:31:19 +0900

    feat: 백엔드 초기설정

---
+++ be/src/test/java/A704/DODREAM/DodreamApplicationTests.java
@@ -0,0 +1,13 @@
+package A704.DODREAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DodreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
