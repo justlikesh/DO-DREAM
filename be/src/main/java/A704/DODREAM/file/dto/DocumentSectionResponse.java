4f35b5017effad49efbc24653bc6072f83e7a56f
김승호 <lpsac@naver.com>
2025-11-02 19:55:54 +0900

    [S13P11A704-98] <Feat> 문서 구조 Repository 및 S3/CF DTO 정의
    - DocumentSection 엔티티에 대한 데이터베이스 접근을 담당하는 DocumentSectionRepository 인터페이스를 새로 추가했습니다.
        - 파일 ID와 레벨을 기준으로 섹션을 조회하는 메서드를 정의했습니다.
    - 클라이언트에 응답하기 위한 DTO 클래스들을 새로 추가했습니다.
        - DocumentSectionResponse: 개별 문서 섹션 정보를 담는 DTO를 정의했습니다.
        - DocumentStructureResponse: 파일의 전체 구조(섹션 목록 포함) 응답을 위한 DTO를 정의했습니다.
        - DownloadUrlResponse: CloudFront 서명된 URL 정보를 클라이언트에 전달하기 위한 DTO를 정의했습니다.

---
+++ be/src/main/java/A704/DODREAM/file/dto/DocumentSectionResponse.java
@@ -0,0 +1,20 @@
+package A704.DODREAM.file.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentSectionResponse {
+    private Long sectionId;
+    private String title;
+    private Integer level;
+    private Integer startPage;
+    private Integer endPage;
+    private Integer fontSize;
+    private String summaryText;
+}
---
+++ be/src/main/java/A704/DODREAM/file/dto/DownloadUrlResponse.java
@@ -0,0 +1,17 @@
+package A704.DODREAM.file.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DownloadUrlResponse {
+    private String downloadUrl;
+    private Long expiresIn; // seconds
+    private String fileName;
+    private Long fileId;
+}
