ffc615e772ca29f32500b71a2a5483f0a023e1e6
양진서 <jinseo_yang@naver.com>
2025-11-03 11:38:31 +0900

    test: MMKV 저장소 테스트 화면 적용

--- fe_app/App.tsx
+++ fe_app/App.tsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import AppNavigator from './src/navigation/AppNavigator';
-import { navigationRef } from './src/navigation/RootNavigation';
-import GlobalVoiceTriggers from './src/components/GlobalVoiceTriggers';
-import { TriggerProvider } from './src/triggers/TriggerContext';
+import MMKVTestScreen from './src/screens/MMKVTestScreen';
 
 export default function App() {
   return (
-    <TriggerProvider>
-      <AppNavigator />
-      <GlobalVoiceTriggers
-        onVoiceCommand={() => {
-          navigationRef.current?.navigate('Question' as never);
-        }}
-      />
+    <>
+      <MMKVTestScreen />
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-    </TriggerProvider>
+    </>
   );
 }
