f0ac7c0128a059531a5c3a0ebffaba8d5fe95c26
김윤수 <rladbstn0425@gmail.com>
2025-11-04 14:10:52 +0900

    refactor: WebConfig 수정

--- be/src/main/java/A704/DODREAM/config/WebConfig.java
+++ be/src/main/java/A704/DODREAM/config/WebConfig.java
@@ -9,11 +9,12 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**")
+    registry.addMapping("/api/**")
         .allowedOrigins("*")
+		.allowedOrigins("https://www.dodream.io.kr")
         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
         .allowedHeaders("*")
-        .allowCredentials(false)
+        .allowCredentials(true)
         .maxAge(3600);
 
   }
