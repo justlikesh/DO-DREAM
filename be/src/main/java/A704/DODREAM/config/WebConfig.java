da68b5f5ccd4f2d39ca7b95a6990ead297dd1fa1
김윤수 <rladbstn0425@gmail.com>
2025-11-04 14:28:26 +0900

    refactor: Cors 에러 수정

--- be/src/main/java/A704/DODREAM/config/WebConfig.java
+++ be/src/main/java/A704/DODREAM/config/WebConfig.java
@@ -6,16 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/api/**")
-        .allowedOrigins("*")
-		.allowedOrigins("http://localhost:5173", "https://www.dodream.io.kr")
-        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-        .allowedHeaders("*")
-        .allowCredentials(true)
-        .maxAge(3600);
-
-  }
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/api/**")
+			.allowedOrigins("http://localhost:5173", "https://www.dodream.io.kr") // ★ "*" 제거
+			.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+			.allowedHeaders("*")
+			.allowCredentials(true)
+			.maxAge(3600);
+	}
 }
